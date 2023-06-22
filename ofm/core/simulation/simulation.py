c4125390899db1346bbaeac320609529686de7a1
sturdy-robot <pedrenriquegg@hotmail.com>
2023-06-22 20:58:53 -0300

    update(simulation): working on a few tests for simulation engine

--- ofm/core/simulation/simulation.py
+++ ofm/core/simulation/simulation.py
@@ -14,6 +14,7 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import random
+from decimal import Decimal, getcontext
 
 from .fixture import Fixture
 from ..football.club import TeamSimulation
@@ -29,43 +30,74 @@ class LiveGame:
         possible_extra_time: bool,
         possible_penalties: bool,
     ):
+        self.minutes = Decimal(0.0)
         self.fixture = fixture
-        self.home_team = home_team
-        self.away_team = away_team
+        self.is_half_time = False
+        self.is_game_over = False
+        self.possible_penalties = possible_penalties
+        self.possible_extra_time = possible_extra_time
         self.attendance = self.calculate_attendance()
-        self.engine = SimulationEngine(possible_penalties, possible_extra_time)
+        self.engine = SimulationEngine(home_team, away_team)
 
     def calculate_attendance(self) -> int:
         pass
 
+    def reset_after_half_time(self):
+        self.is_half_time = False
+        self.minutes += Decimal(0.1)
+
+    def game_is_not_in_break(self) -> bool:
+        if self.minutes == 120.0:
+            if self.possible_penalties and self.engine.is_game_a_draw():
+                self.is_half_time = True
+            else:
+                self.is_game_over = True
+            return False
+        elif self.minutes in [45.0, 105.0]:
+            self.is_half_time = True
+            return False
+        elif self.minutes == 90.0:
+            if self.possible_extra_time and self.engine.is_game_a_draw():
+                self.is_half_time = True
+            else:
+                self.is_game_over = True
+            return False
+        return True
+
     def run(self):
-        while not self.engine.is_game_over and not self.engine.is_half_time:
-            event = self.engine.generate_events()
-            event.calculate_event(self.home_team, self.away_team)
+        while not self.is_game_over and not self.is_half_time:
+            if self.game_is_not_in_break():
+                self.engine.run(self.minutes)
+                self.minutes += Decimal(0.1)
 
 
 class SimulationEngine:
     def __init__(
         self,
-        possible_penalties: bool,
-        possible_extra_time: bool,
+        home_team: TeamSimulation,
+        away_team: TeamSimulation,
     ):
-        self.minutes = 0.0
-        self.is_half_time = False
-        self.is_game_over = False
-        self.possible_penalties = possible_penalties
-        self.possible_extra_time = possible_extra_time
+        getcontext().prec = 5
+        self.home_team = home_team
+        self.away_team = away_team
         self.event_history = []
         self.possession = random.choice(list(Possession))
         self.pitch_position = PitchPosition.MIDFIELD_CENTER
 
-    def generate_events(self) -> SimulationEvent:
-        events = list(EventType)
-        if not self.event_history:
-            event_type = EventType.START_MATCH
-        else:
-            event_type = EventType.PASS
+    def generate_event(self) -> SimulationEvent:
+        pass
 
-        return SimulationEvent(
-            event_type, self.minutes, self.possession, self.pitch_position
-        )
+    def is_game_a_draw(self) -> bool:
+        return self.home_team.score == self.away_team.score
+
+    def get_team_in_possession(self) -> tuple[TeamSimulation, TeamSimulation]:
+        if self.possession == Possession.HOME_TEAM:
+            return self.home_team, self.away_team
+        else:
+            return self.away_team, self.home_team
+
+    def run(self, minutes: Decimal):
+        pass
+        # event = self.generate_event()
+        # attacking_team, defending_team = self.get_team_in_possession(home_team, away_team)
+        # event.calculate_event(attacking_team, defending_team)
---
+++ ofm/tests/test_simulation.py
@@ -0,0 +1,135 @@
+#      Openfoot Manager - A free and open source soccer management simulation
+#      Copyright (C) 2020-2023  Pedrenrique G. Guimarães
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import pytest
+import uuid
+from decimal import Decimal
+
+from ofm.core.db.generators import TeamGenerator
+from ofm.core.simulation.fixture import Fixture
+from ofm.core.simulation.simulation import SimulationEngine, LiveGame
+from ofm.core.football.club import TeamSimulation, Formation
+from .test_teams import get_squads_def, get_confederations_file
+
+
+class MockSimulationEngine:
+    def run(self, minutes: Decimal):
+        pass
+
+
+@pytest.fixture
+def live_game(monkeypatch) -> LiveGame:
+    def get_simulation_engine(*args, **kwargs):
+        return MockSimulationEngine()
+
+    team_gen = TeamGenerator(get_squads_def(), get_confederations_file())
+
+    teams = team_gen.generate()
+    home_team, away_team = teams[0], teams[1]
+    fixture = Fixture(uuid.uuid4(), uuid.uuid4(), home_team.club_id, away_team.club_id, home_team.stadium)
+
+    home_team_formation = Formation(home_team.default_formation)
+    home_team_formation.get_best_players(home_team.squad)
+    home_team_sim = TeamSimulation(home_team, home_team_formation)
+    away_team_formation = Formation(away_team.default_formation)
+    away_team_sim = TeamSimulation(away_team, away_team_formation)
+
+    monkeypatch.setattr(SimulationEngine, 'run', get_simulation_engine)
+
+    return LiveGame(
+        fixture,
+        home_team_sim,
+        away_team_sim,
+        False,
+        False,
+    )
+
+
+def test_game_breaks_in_half_time(live_game):
+    live_game.run()
+    assert live_game.minutes == 45.0
+    assert live_game.is_half_time is True
+
+
+def test_game_breaks_in_90_min(live_game):
+    live_game.run()
+    live_game.reset_after_half_time()
+    live_game.run()
+    assert live_game.minutes == 90.0
+    assert live_game.is_game_over is True
+    assert live_game.is_half_time is False
+
+
+def test_game_breaks_in_extra_time(live_game):
+    live_game.possible_extra_time = True
+    live_game.run()
+    live_game.reset_after_half_time()
+    live_game.run()
+    assert live_game.minutes == 90.0
+    assert live_game.is_game_over is False
+    assert live_game.is_half_time is True
+
+
+def test_game_breaks_in_extra_time_half_time(live_game):
+    live_game.possible_extra_time = True
+    live_game.run()     # first half
+    live_game.reset_after_half_time()
+    live_game.run()     # second half
+    live_game.reset_after_half_time()
+    live_game.run()
+    assert live_game.minutes == 105.0
+    assert live_game.is_game_over is False
+    assert live_game.is_half_time is True
+
+
+def test_game_breaks_after_extra_time(live_game):
+    live_game.possible_extra_time = True
+    live_game.run() # first half
+    live_game.reset_after_half_time()
+    live_game.run() # second half
+    live_game.reset_after_half_time()
+    live_game.run() # first et half
+    live_game.reset_after_half_time()
+    live_game.run()
+    assert live_game.minutes == 120.0
+    assert live_game.is_game_over is True
+    assert live_game.is_half_time is False
+
+
+def test_game_breaks_to_penalty_shootout(live_game):
+    live_game.possible_extra_time = True
+    live_game.possible_penalties = True
+    live_game.run()  # first half
+    live_game.reset_after_half_time()
+    live_game.run()  # second half
+    live_game.reset_after_half_time()
+    live_game.run()  # first et half
+    live_game.reset_after_half_time()
+    live_game.run()
+    assert live_game.minutes == 120.0
+    assert live_game.is_game_over is False
+    assert live_game.is_half_time is True
+
+
+def test_game_breaks_and_does_not_go_to_extra_time(live_game):
+    live_game.possible_extra_time = True
+    live_game.run()     # first half
+    live_game.reset_after_half_time()
+    live_game.engine.home_team.score = 1
+    live_game.engine.away_team.score = 0
+    live_game.run()     # second half
+    assert live_game.minutes == 90.0
+    assert live_game.is_game_over is True
+    assert live_game.is_half_time is False
